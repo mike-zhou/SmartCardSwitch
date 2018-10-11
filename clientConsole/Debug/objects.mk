93044339f47accded8346f32e09ca38367bdc50d
Mike <mike.qx.zhou@gmail.com>
2018-10-11 22:51:43 +1300

    Added Poco libraries and pthread

--- clientConsole/Debug/objects.mk
+++ clientConsole/Debug/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS :=
+LIBS := -lPocoZipd -lPocoNetd -lPocoUtild -lPocoXMLd -lPocoJSONd -lPocoFoundationd -lpthread
 
--- clientConsole/src/clientConsole.cpp
+++ clientConsole/src/clientConsole.cpp
@@ -6,10 +6,95 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
+#include "Poco/Util/ServerApplication.h"
+#include "Poco/Util/Option.h"
+#include "Poco/Util/OptionSet.h"
+#include "Poco/Util/HelpFormatter.h"
+#include "Poco/Task.h"
+#include "Poco/TaskManager.h"
+#include "Poco/DateTimeFormatter.h"
+#include "Poco/Net/SocketAddress.h"
+#include "Poco/Exception.h"
 #include <iostream>
+
 using namespace std;
 
-int main() {
-	cout << "Welcome to the clientConsole" << endl; // prints Welcome to the clientConsole
-	return 0;
-}
+using Poco::Util::Application;
+using Poco::Util::ServerApplication;
+using Poco::Util::Option;
+using Poco::Util::OptionSet;
+using Poco::Util::OptionCallback;
+using Poco::Util::HelpFormatter;
+using Poco::Task;
+using Poco::TaskManager;
+using Poco::DateTimeFormatter;
+
+class Proxy: public ServerApplication
+{
+public:
+	Proxy(): _helpRequested(false)
+	{
+	}
+
+	~Proxy()
+	{
+	}
+
+protected:
+	void initialize(Application& self)
+	{
+		loadConfiguration(); // load default configuration files, if present
+		ServerApplication::initialize(self);
+		logger().information("starting up");
+	}
+
+	void uninitialize()
+	{
+		logger().information("shutting down");
+		ServerApplication::uninitialize();
+	}
+
+	void defineOptions(OptionSet& options)
+	{
+		ServerApplication::defineOptions(options);
+
+		options.addOption(
+			Option("help", "h", "display help information on command line arguments")
+				.required(false)
+				.repeatable(false)
+				.callback(OptionCallback<Proxy>(this, &Proxy::handleHelp)));
+	}
+
+	void handleHelp(const std::string& name, const std::string& value)
+	{
+		_helpRequested = true;
+		displayHelp();
+		stopOptionsProcessing();
+	}
+
+	void displayHelp()
+	{
+		HelpFormatter helpFormatter(options());
+		helpFormatter.setCommand(commandName());
+		helpFormatter.setUsage("OPTIONS");
+		helpFormatter.setHeader("A sample server application that demonstrates some of the features of the Util::ServerApplication class.");
+		helpFormatter.format(std::cout);
+	}
+
+	int main(const ArgVec& args)
+	{
+		if (!_helpRequested)
+		{
+
+
+
+		}
+		return Application::EXIT_OK;
+	}
+
+private:
+	bool _helpRequested;
+};
+
+
+POCO_SERVER_MAIN(Proxy)
