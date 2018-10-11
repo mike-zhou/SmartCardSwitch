ea3494d64fba86b8aae3a4973a203690d01fe72b
Mike <mike.qx.zhou@gmail.com>
2018-10-11 22:30:04 +1300

    New project clientConsole

---
+++ clientConsole/src/clientConsole.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : clientConsole.cpp
+// Author      : Mike Zhou
+// Version     :
+// Copyright   : This software works with proxy to operate Advanced Smart Cards Switch. All of these belongs to Deeply Customized Device Ltd.
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Welcome to the clientConsole" << endl; // prints Welcome to the clientConsole
+	return 0;
+}
