607464d7b6833e505fda026ec8b7aa743cf49cdf
Mike <mike.qx.zhou@gmail.com>
2018-10-13 12:17:09 +1300

    New SmartCardSwitch

---
+++ SmartCardSwitch/src/SmartCardSwitch.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : SmartCardSwitch.cpp
+// Author      : Mike Zhou
+// Version     :
+// Copyright   : This software works with proxy to operate Advanced Smart Cards Switch. All of these belongs to Deeply Customed Device Ltd.
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Welcome to the clientConsole" << endl; // prints Welcome to the clientConsole
+	return 0;
+}
