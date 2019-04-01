a297926f994cc07776fd1f45d8e66b485b54c410
mike-zhou <mike.qx.zhou@gmail.com>
2019-04-02 09:50:51 +1300

    Simplify v4l2grab and convert it to Poco style

--- ImageCapture/src/v4l2grab.cpp
+++ ImageCapture/src/v4l2grab.cpp
@@ -32,12 +32,7 @@
  ***************************************************************************/
 
 // compile with all three access methods
-#if !defined(IO_READ) && !defined(IO_MMAP) && !defined(IO_USERPTR)
-#define IO_READ
-#define IO_MMAP
-#define IO_USERPTR
-#endif
-
+#include <string>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -60,15 +55,38 @@
 #include <signal.h>
 #include <stdint.h>
 #include <inttypes.h>
+#include <iostream>
+
+#include "Poco/Util/ServerApplication.h"
+#include "Poco/Util/Option.h"
+#include "Poco/Util/OptionSet.h"
+#include "Poco/Util/HelpFormatter.h"
+#include "Poco/Task.h"
+#include "Poco/TaskManager.h"
+#include "Poco/DateTimeFormatter.h"
+#include "Poco/Net/SocketAddress.h"
+#include "Poco/Exception.h"
+#include "Poco/Path.h"
+#include "Poco/File.h"
+
+#include "Logger.h"
 
 #include "config.h"
 #include "yuv.h"
 
-#define CLEAR(x) memset (&(x), 0, sizeof (x))
+using Poco::Util::Application;
+using Poco::Util::ServerApplication;
+using Poco::Util::Option;
+using Poco::Util::OptionSet;
+using Poco::Util::OptionCallback;
+using Poco::Util::HelpFormatter;
+using Poco::Task;
+using Poco::TaskManager;
+using Poco::DateTimeFormatter;
 
-#ifndef VERSION
-#define VERSION "unknown"
-#endif
+Logger * pLogger;
+
+#define CLEAR(x) memset (&(x), 0, sizeof (x))
 
 typedef enum {
         IO_METHOD_READ,
@@ -83,47 +101,13 @@ static int              fd              = -1;
 struct buffer *         buffers         = NULL;
 
 // global settings
-static unsigned int width = 640;
-static unsigned int height = 480;
-static unsigned int fps = 30;
-static int continuous = 0;
-static unsigned char jpegQuality = 70;
-static char* jpegFilename = NULL;
-static char* jpegFilenamePart = NULL;
-static char* deviceName = "/dev/video0";
-
-static const char* const continuousFilenameFmt = "%s_%010"PRIu32"_%"PRId64".jpg";
-
-/**
-SIGINT interput handler
-*/
-void StopContCapture(int sig_id) {
-	printf("stoping continuous capture\n");
-	continuous = 0;
-}
-
-void InstallSIGINTHandler() {
-	struct sigaction sa;
-	CLEAR(sa);
-	
-	sa.sa_handler = StopContCapture;
-	if(sigaction(SIGINT, &sa, 0) != 0)
-	{
-		fprintf(stderr,"could not install SIGINT handler, continuous capture disabled");
-		continuous = 0;
-	}
-}
-
-/**
-	Print error message and terminate programm with EXIT_FAILURE return code.
-
-	\param s error message to print
-*/
-static void errno_exit(const char* s)
-{
-	fprintf(stderr, "%s error %d, %s\n", s, errno, strerror(errno));
-	exit(EXIT_FAILURE);
-}
+static unsigned int _width;
+static unsigned int _height;
+static int _fps;
+static int _continuous;
+static unsigned char _jpegQuality;
+static std::string _deviceFile;
+static unsigned char * _pYUV422Buffer;
 
 /**
 	Do ioctl and retry if error was EINTR ("A signal was caught during the ioctl() operation."). Parameters are the same as on ioctl.
@@ -158,7 +142,8 @@ static void jpegWrite(unsigned char* img, char* jpegFilename)
 
 	// try to open file for saving
 	if (!outfile) {
-		errno_exit("jpeg");
+		pLogger->LogError("jpegWrite unable to open jpeg file");
+		return;
 	}
 
 	// create jpeg data
@@ -167,15 +152,15 @@ static void jpegWrite(unsigned char* img, char* jpegFilename)
 	jpeg_stdio_dest(&cinfo, outfile);
 
 	// set image parameters
-	cinfo.image_width = width;
-	cinfo.image_height = height;
+	cinfo.image_width = _width;
+	cinfo.image_height = _height;
 	cinfo.input_components = 3;
 	cinfo.in_color_space = JCS_YCbCr;
 
 	// set jpeg compression parameters to default
 	jpeg_set_defaults(&cinfo);
 	// and then adjust quality setting
-	jpeg_set_quality(&cinfo, jpegQuality, TRUE);
+	jpeg_set_quality(&cinfo, _jpegQuality, TRUE);
 
 	// start compress
 	jpeg_start_compress(&cinfo, TRUE);
@@ -199,27 +184,20 @@ static void jpegWrite(unsigned char* img, char* jpegFilename)
 /**
 	process image read
 */
-static void imageProcess(const void* p, struct timeval timestamp)
+static void imageProcess(const void* p)
 {
-	//timestamp.tv_sec
-	//timestamp.tv_usec
+	char fileName[128];
 	unsigned char* src = (unsigned char*)p;
-	unsigned char* dst = (unsigned char*)malloc(width*height*3*sizeof(char));
+	Poco::Timestamp curTime;
 
-	YUV420toYUV444(width, height, src, dst);
 
-	if(continuous==1) {
-		static uint32_t img_ind = 0;
-		int64_t timestamp_long;
-		timestamp_long = timestamp.tv_sec*1e6 + timestamp.tv_usec;
-		sprintf(jpegFilename,continuousFilenameFmt,jpegFilenamePart,img_ind++,timestamp_long);
 
-	}
+	YUV420toYUV444(_width, _height, src, _pYUV422Buffer);
+
+	sprintf(fileName, "%s", Poco::DateTimeFormatter::format(curTime, "%Y-%m-%d_%h-%M-%S.%i.jpg").c_str());
+
 	// write jpeg
-	jpegWrite(dst,jpegFilename);
-
-	// free temporary image
-	free(dst);
+	jpegWrite(_pYUV422Buffer, fileName);
 }
 
 /**
@@ -237,17 +215,11 @@ static int frameRead(void)
 				// fall through
 
 			default:
-				errno_exit("read");
+				throw Poco::Exception("failed to read from device");
 		}
 	}
 
-	struct timespec ts;
-	struct timeval timestamp;
-	clock_gettime(CLOCK_MONOTONIC,&ts);
-	timestamp.tv_sec = ts.tv_sec;
-	timestamp.tv_usec = ts.tv_nsec/1000;
-
-	imageProcess(buffers[0].start,timestamp);
+	imageProcess(buffers[0].start);
 
 	return 1;
 }
@@ -255,68 +227,39 @@ static int frameRead(void)
 /**
 	mainloop: read frames and process them
 */
-static void mainLoop(void)
+static void captureImage(void)
 {	
-	int count;
-	unsigned int numberOfTimeouts;
+	for (;;)
+	{
+		fd_set fds;
+		struct timeval tv;
+		int r;
 
-	numberOfTimeouts = 0;
-	count = 3;
+		FD_ZERO(&fds);
+		FD_SET(fd, &fds);
 
-	while (count-- > 0) {
-		for (;;) {
-			fd_set fds;
-			struct timeval tv;
-			int r;
+		/* Timeout. */
+		tv.tv_sec = 1;
+		tv.tv_usec = 0;
 
-			FD_ZERO(&fds);
-			FD_SET(fd, &fds);
+		r = select(fd + 1, &fds, NULL, NULL, &tv);
 
-			/* Timeout. */
-			tv.tv_sec = 1;
-			tv.tv_usec = 0;
+		if (-1 == r) {
+			if (EINTR == errno)
+				continue;
 
-			r = select(fd + 1, &fds, NULL, NULL, &tv);
-
-			if (-1 == r) {
-				if (EINTR == errno)
-					continue;
-
-				errno_exit("select");
-			}
-
-			if (0 == r) {
-				if (numberOfTimeouts <= 0) {
-					count++;
-				} else {
-					fprintf(stderr, "select timeout\n");
-					exit(EXIT_FAILURE);
-				}
-			}
-			if(continuous == 1) {
-				count = 3;
-			}
-
-			if (frameRead())
-				break;
-
-			/* EAGAIN - continue select loop. */
+			throw Poco::Exception("error in waiting device file");
 		}
+
+		if (0 == r)
+		{
+			throw Poco::Exception("device time out");
+		}
+
+		frameRead();
+
+		break;
 	}
-}
-
-/**
-	stop capturing
-*/
-static void captureStop(void)
-{
-}
-
-/**
-  start capturing
-*/
-static void captureStart(void)
-{
 }
 
 static void deviceUninit(void)
@@ -329,17 +272,16 @@ static void readInit(unsigned int buffer_size)
 {
 	buffers = (struct buffer *)calloc(1, sizeof(*buffers));
 
-	if (!buffers) {
-		fprintf(stderr, "Out of memory\n");
-		exit(EXIT_FAILURE);
+	if (!buffers)
+	{
+		throw Poco::Exception("Out of memory");
 	}
 
 	buffers[0].length = buffer_size;
 	buffers[0].start = malloc(buffer_size);
 
 	if (!buffers[0].start) {
-		fprintf (stderr, "Out of memory\n");
-		exit(EXIT_FAILURE);
+		throw Poco::Exception("Out of memory");
 	}
 }
 
@@ -359,21 +301,18 @@ static void deviceInit(void)
 
 	if (-1 == xioctl(fd, VIDIOC_QUERYCAP, &cap)) {
 		if (EINVAL == errno) {
-			fprintf(stderr, "%s is no V4L2 device\n",deviceName);
-			exit(EXIT_FAILURE);
+			throw Poco::Exception("none v4l2 device");
 		} else {
-			errno_exit("VIDIOC_QUERYCAP");
+			throw Poco::Exception("error in QUERYCAP: " + std::to_string(errno));
 		}
 	}
 
 	if (!(cap.capabilities & V4L2_CAP_VIDEO_CAPTURE)) {
-		fprintf(stderr, "%s is no video capture device\n",deviceName);
-		exit(EXIT_FAILURE);
+		throw Poco::Exception("none video capture device");
 	}
 
 	if (!(cap.capabilities & V4L2_CAP_READWRITE)) {
-		fprintf(stderr, "%s does not support read i/o\n",deviceName);
-		exit(EXIT_FAILURE);
+		throw Poco::Exception("device doesn't support READWRITE");
 	}
 
 	/* Select video input, video standard and tune here. */
@@ -403,42 +342,46 @@ static void deviceInit(void)
 
 	// v4l2_format
 	fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-	fmt.fmt.pix.width = width;
-	fmt.fmt.pix.height = height;
+	fmt.fmt.pix.width = _width;
+	fmt.fmt.pix.height = _height;
 	fmt.fmt.pix.field = V4L2_FIELD_INTERLACED;
 	fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_YUV420;
 
-	if (-1 == xioctl(fd, VIDIOC_S_FMT, &fmt))
-		errno_exit("VIDIOC_S_FMT");
+	if (-1 == xioctl(fd, VIDIOC_S_FMT, &fmt)){
+		throw Poco::Exception("VIDIOC_S_FMT");
+	}
 
 	if (fmt.fmt.pix.pixelformat != V4L2_PIX_FMT_YUV420) {
-		fprintf(stderr,"Libv4l didn't accept YUV420 format. Can't proceed.\n");
-		exit(EXIT_FAILURE);
+		throw Poco::Exception("Libv4l didn't accept YUV420 format. Can't proceed");
 	}
 
 	/* Note VIDIOC_S_FMT may change width and height. */
-	if (width != fmt.fmt.pix.width) {
-		width = fmt.fmt.pix.width;
-		fprintf(stderr,"Image width set to %i by device %s.\n", width, deviceName);
+	if (_width != fmt.fmt.pix.width)
+	{
+		_width = fmt.fmt.pix.width;
+		pLogger->LogInfo("Image width set to " + std::to_string(_width) + " by device: " + _deviceFile);
 	}
 
-	if (height != fmt.fmt.pix.height) {
-		height = fmt.fmt.pix.height;
-		fprintf(stderr,"Image height set to %i by device %s.\n", height, deviceName);
+	if (_height != fmt.fmt.pix.height)
+	{
+		_height = fmt.fmt.pix.height;
+		pLogger->LogInfo("Image height set to " + std::to_string(_height) + " by device: " + _deviceFile);
 	}
 	
-  /* If the user has set the fps to -1, don't try to set the frame interval */
-  if (fps != -1)
-  {
-    CLEAR(frameint);
-    
-    /* Attempt to set the frame interval. */
-    frameint.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    frameint.parm.capture.timeperframe.numerator = 1;
-    frameint.parm.capture.timeperframe.denominator = fps;
-    if (-1 == xioctl(fd, VIDIOC_S_PARM, &frameint))
-      fprintf(stderr,"Unable to set frame interval.\n");
-  }
+	/* If the user has set the fps to -1, don't try to set the frame interval */
+	if (_fps != -1)
+	{
+		CLEAR(frameint);
+
+		/* Attempt to set the frame interval. */
+		frameint.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+		frameint.parm.capture.timeperframe.numerator = 1;
+		frameint.parm.capture.timeperframe.denominator = _fps;
+		if (-1 == xioctl(fd, VIDIOC_S_PARM, &frameint))
+		{
+		   pLogger->LogError("failed to set fps to: " + std::to_string(_fps));
+		}
+	}
 
 	/* Buggy driver paranoia. */
 	min = fmt.fmt.pix.width * 2;
@@ -457,7 +400,9 @@ static void deviceInit(void)
 static void deviceClose(void)
 {
 	if (-1 == v4l2_close(fd))
-		errno_exit("close");
+	{
+		pLogger->LogError("failed to close device file");
+	}
 
 	fd = -1;
 }
@@ -470,174 +415,220 @@ static void deviceOpen(void)
 	struct stat st;
 
 	// stat file
-	if (-1 == stat(deviceName, &st)) {
-		fprintf(stderr, "Cannot identify '%s': %d, %s\n", deviceName, errno, strerror(errno));
-		exit(EXIT_FAILURE);
+	if (-1 == stat(_deviceFile.c_str(), &st)) {
+		throw Poco::Exception("failed to retrieve device stat: " + _deviceFile);
 	}
 
 	// check if its device
 	if (!S_ISCHR(st.st_mode)) {
-		fprintf(stderr, "%s is no device\n", deviceName);
-		exit(EXIT_FAILURE);
+		throw Poco::Exception(" none video device: " + _deviceFile);
 	}
 
 	// open device
-	fd = v4l2_open(deviceName, O_RDWR /* required */ | O_NONBLOCK, 0);
+	fd = v4l2_open(_deviceFile.c_str(), O_RDWR /* required */ | O_NONBLOCK, 0);
 
-	// check if opening was successfull
+	// check if opening was successful
 	if (-1 == fd) {
-		fprintf(stderr, "Cannot open '%s': %d, %s\n", deviceName, errno, strerror(errno));
-		exit(EXIT_FAILURE);
+		throw Poco::Exception("failed to open device: " + _deviceFile);;
 	}
 }
 
-/**
-	print usage information
-*/
-static void usage(FILE* fp, int argc, char** argv)
+class CaptureTask: public Poco::Task
 {
-	fprintf(fp,
-		"Usage: %s [options]\n\n"
-		"Options:\n"
-		"-d | --device name   Video device name [/dev/video0]\n"
-		"-h | --help          Print this message\n"
-		"-o | --output        Set JPEG output filename\n"
-		"-q | --quality       Set JPEG quality (0-100)\n"
-		"-m | --mmap          Use memory mapped buffers\n"
-		"-r | --read          Use read() calls\n"
-		"-u | --userptr       Use application allocated buffers\n"
-		"-W | --width         Set image width\n"
-		"-H | --height        Set image height\n"
-		"-I | --interval      Set frame interval (fps) (-1 to skip)\n"
-		"-c | --continuous    Do continous capture, stop with SIGINT.\n"
-		"-v | --version       Print version\n"
-		"",
-		argv[0]);
+public:
+	CaptureTask():Task("ImageCapture") {}
+	
+private:
+	void runTask()
+	{
+		_pYUV422Buffer = (unsigned char*)malloc(_width*_height*3*sizeof(char));
+		if(NULL == _pYUV422Buffer) {
+			pLogger->LogError("CaptureTask failed to allocate memory");
+			return;
+		}
+
+		try
+		{
+			// open and initialize device
+			deviceOpen();
+			deviceInit();
+
+			while(1)
+			{
+				if(isCancelled())
+				{
+					break;
+				}
+				else
+				{
+					// process frames
+					captureImage();
+				}
+			}
+
+			// close device
+			deviceUninit();
+			deviceClose();
+		}
+		catch(Poco::Exception &e)
+		{
+			pLogger->LogError("CaptureTask exception: " + e.displayText());
+		}
+		catch(std::exception & e)
+		{
+			pLogger->LogError("CaptureTask exception: " + std::string(e.what()));
+		}
+		catch(...)
+		{
+			pLogger->LogError("CaptureTask unknown exception");
+		}
+
+		if(_pYUV422Buffer != NULL) {
+			free(_pYUV422Buffer);
+		}
+
+		pLogger->LogInfo("CaptureTask terminates");
 	}
-
-static const char short_options [] = "d:ho:q:mruW:H:I:vc";
-
-static const struct option
-long_options [] = {
-	{ "device",     required_argument,      NULL,           'd' },
-	{ "help",       no_argument,            NULL,           'h' },
-	{ "output",     required_argument,      NULL,           'o' },
-	{ "quality",    required_argument,      NULL,           'q' },
-	{ "mmap",       no_argument,            NULL,           'm' },
-	{ "read",       no_argument,            NULL,           'r' },
-	{ "userptr",    no_argument,            NULL,           'u' },
-	{ "width",      required_argument,      NULL,           'W' },
-	{ "height",     required_argument,      NULL,           'H' },
-	{ "interval",   required_argument,      NULL,           'I' },
-	{ "version",	no_argument,		NULL,		'v' },
-	{ "continuous",	no_argument,		NULL,		'c' },
-	{ 0, 0, 0, 0 }
 };
 
-int main(int argc, char **argv)
+class ImageCapture: public ServerApplication
 {
+public:
+	ImageCapture(): _helpRequested(false)
+	{
+	}
 
-	for (;;) {
-		int index, c = 0;
+	~ImageCapture()
+	{
+	}
 
-		c = getopt_long(argc, argv, short_options, long_options, &index);
+protected:
+	void initialize(Application& self)
+	{
+		loadConfiguration(); // load default configuration files, if present
+		ServerApplication::initialize(self);
+		logger().information("starting up");
+	}
 
-		if (-1 == c)
-			break;
+	void uninitialize()
+	{
+		logger().information("shutting down");
+		ServerApplication::uninitialize();
+	}
 
-		switch (c) {
-			case 0: /* getopt_long() flag */
-				break;
+	void defineOptions(OptionSet& options)
+	{
+		ServerApplication::defineOptions(options);
 
-			case 'd':
-				deviceName = optarg;
-				break;
+		options.addOption(
+			Option("help", "h", "display help information on command line arguments")
+				.required(false)
+				.repeatable(false)
+				.callback(OptionCallback<ImageCapture>(this, &ImageCapture::handleHelp)));
+	}
 
-			case 'h':
-				// print help
-				usage(stdout, argc, argv);
-				exit(EXIT_SUCCESS);
+	void handleHelp(const std::string& name, const std::string& value)
+	{
+		_helpRequested = true;
+		displayHelp();
+		stopOptionsProcessing();
+	}
 
-			case 'o':
-				// set jpeg filename
-				jpegFilename = optarg;
-				break;
+	void displayHelp()
+	{
+		HelpFormatter helpFormatter(options());
+		helpFormatter.setCommand(commandName());
+		helpFormatter.setUsage("OPTIONS");
+		helpFormatter.setHeader("A sample server application that demonstrates some of the features of the Util::ServerApplication class.");
+		helpFormatter.format(std::cout);
+	}
 
-			case 'q':
-				// set jpeg quality
-				jpegQuality = atoi(optarg);
-				break;
+	int main(const ArgVec& args)
+	{
+		if (!_helpRequested)
+		{
+			TaskManager tmLogger;
+			TaskManager tm;
+			std::string logFolder;
+			std::string logFile;
+			std::string logFileSize;
+			std::string logFileAmount;
 
-			case 'W':
-				// set width
-				width = atoi(optarg);
-				break;
+			//use the designated configuration if it exist
+			if(args.size() > 0)
+			{
+				std::string configFile = args[0];
+				Poco::Path path(configFile);
 
-			case 'H':
-				// set height
-				height = atoi(optarg);
-				break;
-				
-			case 'I':
-				// set fps
-				fps = atoi(optarg);
-				break;
+				if(path.isFile())
+				{
+					Poco::File file(path);
 
-			case 'c':
-				// set flag for continuous capture, interuptible by sigint
-				continuous = 1;
-				InstallSIGINTHandler();
-				break;
-				
+					if(file.exists() && file.canRead()) {
+						loadConfiguration(configFile);
+					}
+				}
+			}
 
-			case 'v':
-				printf("Version: %s\n", VERSION);
-				exit(EXIT_SUCCESS);
-				break;
+			try
+			{
+				//logs
+				logFolder = config().getString("log_file_folder", "./logs/ImageCapture");
+				logFile = config().getString("log_file_name", "ImageCapture");
+				logFileSize = config().getString("log_file_size", "1M");
+				logFileAmount = config().getString("log_file_amount", "10");
 
-			default:
-				usage(stderr, argc, argv);
-				exit(EXIT_FAILURE);
+				_deviceFile = config().getString("device_file", "/dev/video0");
+				_width = config().getUInt("width", 640);
+				_height = config().getUInt("height", 480);
+				_fps = config().getInt("fps", 30);
+				_jpegQuality = config().getUInt("quality", 70);
+				_continuous = config().getUInt("continuous", 0);
+			}
+			catch(Poco::NotFoundException& e)
+			{
+				logger().error("Config NotFoundException: " + e.displayText());
+			}
+			catch(Poco::SyntaxException& e)
+			{
+				logger().error("Config SyntaxException: " + e.displayText());
+			}
+			catch(Poco::Exception& e)
+			{
+				logger().error("Config Exception: " + e.displayText());
+			}
+			catch(...)
+			{
+				logger().error("Config unknown exception");
+			}
+
+			//start the logger
+			pLogger = new Logger(logFolder, logFile, logFileSize, logFileAmount);
+			pLogger->CopyToConsole(true);
+			tmLogger.start(pLogger);
+			pLogger->LogInfo("**** ImageCapture version 1.0.0 ****");
+
+
+			//start the CaptureTask
+			CaptureTask * pTask = new CaptureTask();
+			tm.start(pTask);
+
+			waitForTerminationRequest();
+
+			//stop tasks
+			pLogger->LogInfo("**** ImageCapture Exiting ****");
+			tm.cancelAll();
+			tm.joinAll();
+			//stop logger
+			tmLogger.cancelAll();
+			tmLogger.joinAll();
 		}
+		return Application::EXIT_OK;
 	}
 
-	// check for need parameters
-	if (!jpegFilename) {
-		fprintf(stderr, "You have to specify JPEG output filename!\n\n");
-		usage(stdout, argc, argv);
-		exit(EXIT_FAILURE);
-	}
-	
-	if(continuous == 1) {
-		int max_name_len = snprintf(NULL,0,continuousFilenameFmt,jpegFilename,UINT32_MAX,INT64_MAX);
-		jpegFilenamePart = jpegFilename;
-		jpegFilename = (char*)calloc(max_name_len+1,sizeof(char));
-		strcpy(jpegFilename,jpegFilenamePart);
-	}
-	
+private:
+	bool _helpRequested;
+};
 
-	// open and initialize device
-	deviceOpen();
-	deviceInit();
 
-	// start capturing
-	captureStart();
-
-	// process frames
-	mainLoop();
-
-	// stop capturing
-	captureStop();
-
-	// close device
-	deviceUninit();
-	deviceClose();
-
-	if(jpegFilenamePart != 0){ 
-		free(jpegFilename);
-	}
-
-	exit(EXIT_SUCCESS);
-
-	return 0;
-}
+POCO_SERVER_MAIN(ImageCapture)
