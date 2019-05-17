e54f524947d28955f132dacfa0a3ebd99e6b1046
Mike <mike.qx.zhou@gmail.com>
2019-05-18 01:04:35 +1200

    Temporary save
    (cherry picked from commit 0e461b8b5ef645416aeee78dc4f30aeb0228b43b)

--- nodejs/scripts/cardSlotMapping.js
+++ nodejs/scripts/cardSlotMapping.js
@@ -1,17 +1,15 @@
 var globalCardSlotMappings;
 
-function updateCardSlotMapping(mapping)
-{
+function updateCardSlotMappingTable(mapping) {
     var html = "<table class=\"cardSlotMappingTable\">";
 
     //header
-    html = html + "<tr><th>Card Name<th><th>Slot number</th><th>Action</th></tr>";
+    html = html + "<tr><th>Card Name</th><th>Slot</th><th>Action</th></tr>";
 
     //mappings
-    for(var i=0; i<mapping.length; i++) 
-    {
+    for (var i = 0; i < mapping.length; i++) {
         var row;
-        
+
         row = "<tr>";
         row = row + "<td>" + mapping[i].cardName + "</td>";
         row = row + "<td>" + mapping[i].slotNumber + "</td>";
@@ -26,48 +24,48 @@ function updateCardSlotMapping(mapping)
     document.getElementById("mappingContent_details").innerHTML = html;
 }
 
-function onCardSlotMappingArrived(mappings)
-{
-    globalCardSlotMappings = mappings;
+function onCardSlotMappingArrived(mappings) {
+    globalCardSlotMappings = mappings; //save to global variable.
     var names = [];
     var activeMapping;
 
-    if((globalCardSlotMappings === undefined) ||
+    if ((globalCardSlotMappings === undefined) ||
         (!Array.isArray(globalCardSlotMappings)) ||
-        (globalCardSlotMappings.lenth == 0)) 
-    {
+        (globalCardSlotMappings.lenth == 0)) {
         console.log("onCardSlotMappingArrived illegal mapping: " + mappings);
         return;
     }
 
-    for(var i=0; i<globalCardSlotMappings.length; i++) 
-    {
+    for (var i = 0; i < globalCardSlotMappings.length; i++) {
         var cur = globalCardSlotMappings[i];
         names[i] = cur.name;
-        if(cur.active == true) {
+        if (cur.active == true) {
             activeMapping = cur.name;
-            updateCardSlotMapping(cur.mapping);
+            updateCardSlotMappingTable(cur.mapping);
         }
     }
 
-
-    document.getElementById("mappingSelect_activeMapping").text = activeMapping;
+    document.getElementById("mappingSelect_activeMapping").innerText = activeMapping;
 
     var selection = document.getElementById("mappingSelect_mappingSet");
-    for(var i=0; i<selection.options.length; ) 
-    {
+    for (var i = 0; i < selection.options.length;) {
         selection.options.remove(0);
     }
-    for(var i=0; i<names.length; i++) 
-    {
+    for (var i = 0; i < names.length; i++) {
         var option = document.createElement("option");
         option.text = names[i];
-        selection.options.add(option, i+1);
+        selection.options.add(option, i);
+    }
+
+    var slotSelection = document.getElementById("mappingModify_slotSelection");
+    for (var i = 0; i < 60; i++) {
+        var option = document.createElement("option");
+        option.text = i;
+        slotSelection.options.add(option, i);
     }
 }
 
-function askForCardSlotMapping()
-{
+function askForCardSlotMapping() {
     var xhr = new XMLHttpRequest();
     xhr.responseType = "json";
     xhr.open('POST', '/getCardSlotMappings');
@@ -93,92 +91,69 @@ function askForCardSlotMapping()
 
 document.addEventListener("DOMContentLoaded", askForCardSlotMapping);
 
-function onElementClicked() {
+function loadMapping(mappingName)
+{
+    for(var i=0; i<globalCardSlotMappings.length; i++) {
+        var element = globalCardSlotMappings[i];
+        if(element.name == mappingName) {
+            updateCardSlotMappingTable(element.mapping);
+            break;
+        }
+    }
+}
+
+function addCardSlotMapping(mappingName, cardName, slotNumber)
+{
+    for(var i=0; i<globalCardSlotMappings.length; i++) {
+        var element = globalCardSlotMappings[i];
+        if(element.name == mappingName) {
+            var item = {};
+            item["cardName"] = cardName;
+            item["slotNumber"] = slotNumber;
+            element.mapping.push(item);
+            updateCardSlotMappingTable(element.mapping);
+            break;
+        }
+    }
+}
+
+function onElementClicked() 
+{
+    //element id is in the format of group_action_XXX
     var elementId = document.activeElement.id;
     var paraArray = elementId.split("_");
 
-    var device = paraArray[0];
-    if (device === "stepper") {
-        var index = paraArray[1];
-        var action = paraArray[2];
-
-        if (action === "decrease") {
-            onStepperDecrease(device + index);
-        } else if (action === "decrease128") {
-            moveStepper(device + index, false, 128);
-        } else if (action === "decrease64") {
-            moveStepper(device + index, false, 64);
-        } else if (action === "decrease32") {
-            moveStepper(device + index, false, 32);
-        } else if (action === "decrease16") {
-            moveStepper(device + index, false, 16);
-        } else if (action === "decrease8") {
-            moveStepper(device + index, false, 8);
-        } else if (action === "decrease4") {
-            moveStepper(device + index, false, 4);
-        } else if (action === "decrease1") {
-            moveStepper(device + index, false, 1);
-        } else if (action === "increase") {
-            onStepperIncrease(device + index);
-        } else if (action === "increase128") {
-            moveStepper(device + index, true, 128);
-        } else if (action === "increase64") {
-            moveStepper(device + index, true, 64);
-        } else if (action === "increase32") {
-            moveStepper(device + index, true, 32);
-        } else if (action === "increase16") {
-            moveStepper(device + index, true, 16);
-        } else if (action === "increase8") {
-            moveStepper(device + index, true, 8);
-        } else if (action === "increase4") {
-            moveStepper(device + index, true, 4);
-        } else if (action === "increase1") {
-            moveStepper(device + index, true, 1);
-        } else if (action === "configMovement") {
-            onStepperConfigMovement(index);
-        } else if (action === "configHome") {
-            onStepperConfigHome(index);
-        } else if (action === "configForwardClockwise") {
-            onStepperConfigForwardClockwise(index);
-        } else {
-            alert("onElementClicked unknown action type: " + elementId);
+    var group = paraArray[0];
+    if (group === "mappingSelect") 
+    {
+        var action = paraArray[1];
+        if(action === "choose") {
+            var index =  document.getElementById("mappingSelect_mappingSet").selectedIndex;
+            var mappingName = document.getElementById("mappingSelect_mappingSet").options[index].text;
+            console.log("onElementClicked load mapping: " + mappingName);
+            loadMapping(mappingName);
         }
-    } else if (device === "bdc") {
-        var index = parseInt(paraArray[1]);
-        var action = paraArray[2];
-
-        if (action === "Forward") {
-            setBdc(index, 0);
-        } else if (action === "Reverse") {
-            setBdc(index, 1);
-        } else if (action === "Release") {
-            setBdc(index, 2);
-        } else {
-            console.log("Error: wrong bdc action: " + action);
-        }
-    } else if (device === "coordinateItem") {
-        var type = paraArray[1];
-        var index = parseInt(paraArray[2]);
-        onCoordinateItem(type, index);
-    } else if (device === "smartCardOffset") {
-        var index;
-        var type = paraArray[1];
-
-        if ((type === "index") || (type === "value")) {
-            index = parseInt(paraArray[2]);
-        }
-        onSmartCardOffset(type, index);
-    } else if (device === "power") {
-        var target = paraArray[1];
-        var action = paraArray[2];
-        onPower(target, action === "on");
-    } else if (device === "iFinger") {
-        var keyIndex = paraArray[2];
-        onKey(keyIndex);
-    } else if (elementId === "coordinate_save") {
-        saveCoordinate();
-    } else if (elementId === "deviceQuery") {
-        queryDevice();
     }
+    else if(group === "mappingContent") 
+    {
+        var action = paraArray[1];
+        if(action === "delete") {
 
+        }
+    }
+    else if(group === "mappingModify")
+    {
+        var action = paraArray[1];
+        if(action === "add") {
+            var cardName = document.getElementById("mappingModify_cardName").value;
+            var slotNumber = document.getElementById("mappingModify_slotSelection").selectedIndex;
+            var mappingName = document.getElementById("mappingSelect_activeMapping").innerText;
+
+            console.log("onElementClicked add " + cardName + ":" + slotNumber + " to " + mappingName);
+            addCardSlotMapping(mappingName, cardName, slotNumber);
+        }
+        else if(action === "save") {
+
+        }
+    }
 }
