b5d8f5f6be808455d4b1f5100f1cddef000b0b8f
Mike <mike.qx.zhou@gmail.com>
2019-06-08 10:44:13 +1200

    Remove clientConsole and scsClient

--- clientConsole/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lscsClient -lPocoNetd -lPocoUtild -lPocoXMLd -lPocoJSONd -lPocoFoundationd -lpthread -lLogger
-
