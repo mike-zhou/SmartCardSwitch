b1e674d5dcbd81719877732d8c5a38834694c298
mike-zhou <mike.qx.zhou@gmail.com>
2021-04-16 13:02:07 +1200

    Add CDataExchange

---
+++ proxy/src/CDataExchange.h
@@ -0,0 +1,174 @@
+#ifndef _CDATAEXCHANGE_H_
+#define _CDATAEXCHANGE_H_
+
+#include <vector>
+#include "Poco/Mutex.h"
+#include "Poco/Timestamp.h"
+
+#include "CrcCcitt.h";
+
+class IDataExchangeObserver
+{
+public:
+    virtual void OnReply(unsigned char * pReply, unsigned int length) = 0;
+};
+
+class IPacketSender
+{
+public:
+    virtual unsigned int SendPacketData(unsigned char * pData, unsigned int length) = 0;
+};
+
+class IMonitorObserver
+{
+public:
+    virtual void OnMonitorInfo(unsigned char * pInfo, unsigned int length) = 0;
+};
+
+/**
+ * this class accept a block of data, devides it to different packets, then sends out packets one by one
+ * it also merges replied packets to a data block and sends it to the observer.
+ * Poll() needs to be called repeatedly to drive this class
+ */
+class CDataExchange
+{
+public:
+    CDataExchange();
+    void Poll();
+
+    unsigned int SendData(unsigned char * pData, unsigned int length);
+    void ClearData();
+    void SetDataObserver(IDataExchangeObserver * pObserver);
+
+    void SetPacketSender(IPacketSender * pSender);
+    void OnPacketReply(unsigned char * pData, unsigned int length);
+
+    void SetMonitorObserver(IMonitorObserver * pObserver);
+
+private:
+    /*********************************************************
+    * Data exchange stages
+    **********************************************************/
+    #define SCS_PACKET_MAX_LENGTH 64
+    #define SCS_DATA_PACKET_TAG 0xDD
+    /************************************************************************/
+    /* 
+    Data packet structure:
+        0xDD		// 1 byte
+        id			// 1 byte: 0, 1 to 0xFE
+        dataLength	// 1 byte
+        data bytes	// bytes of dataLength
+        crcLow		// 1 byte
+        crcHigh		// 1 byte                                                                    
+    */
+    /************************************************************************/
+    #define SCS_ACK_PACKET_TAG 0xAA
+    /************************************************************************/
+    /*          
+    Acknowledge packet structure:
+        0xAA		// 1 byte
+        id			// 1 byte: 0, 1 to 0xFE
+        crcLow      // 1 byte
+        crcHigh		// 1 byte                                           
+    */
+    /************************************************************************/
+    #define SCS_DATA_PACKET_STAFF_LENGTH 5 //tag, id, dataLength, crcLow, crcHigh
+    #define SCS_DATA_MAX_LENGTH (SCS_PACKET_MAX_LENGTH - SCS_DATA_PACKET_STAFF_LENGTH)
+    #define SCS_ACK_PACKET_LENGTH 4
+    #define SCS_DATA_INPUT_TIMEOUT 50 //milliseconds
+    #define SCS_DATA_OUTPUT_TIMEOUT 200 //milliseconds
+    #define SCS_INITIAL_PACKET_ID 0 //this id is used only once at the launch of application
+    #define SCS_INVALID_PACKET_ID 0xFF
+
+    enum SCS_Input_Stage_State
+    {
+        SCS_INPUT_IDLE = 0,
+        SCS_INPUT_RECEIVING
+    };
+
+    struct SCS_Input_Stage
+    {
+        enum SCS_Input_Stage_State state;
+        unsigned char packetBuffer[SCS_PACKET_MAX_LENGTH];
+        unsigned char byteAmount;
+        unsigned short timeStamp;
+        unsigned char prevDataPktId;
+    };
+
+    enum SCS_Output_Stage_State
+    {
+        SCS_OUTPUT_IDLE = 0,
+        SCS_OUTPUT_SENDING_DATA,
+        SCS_OUTPUT_SENDING_ACK,
+        SCS_OUTPUT_SENDING_DATA_PENDING_ACK,
+        SCS_OUTPUT_WAIT_ACK,
+        SCS_OUTPUT_SENDING_ACK_WAIT_ACK
+    };
+
+    struct SCS_Output_Stage
+    {
+        enum SCS_Output_Stage_State state;
+        //data packet
+        unsigned char dataPktBuffer[SCS_PACKET_MAX_LENGTH];
+        unsigned char dataPktSendingIndex; //index of byte to be sent
+        unsigned char currentDataPktId;
+        unsigned char ackedDataPktId;
+        unsigned short dataPktTimeStamp;
+        //acknowledge packet
+        unsigned char ackPktBuffer[SCS_ACK_PACKET_LENGTH];
+        unsigned char ackPktSendingIndex; //index of byte to be sent
+    };
+
+    SCS_Input_Stage _scsInputStage;
+    unsigned short _scsInputTimeOut;
+    SCS_Output_Stage _scsOutputStage;
+    unsigned short _scsOutputTimeout;
+
+    #define MONITOR_OUTPUT_BUFFER_LENGTH_MASK 0xFF
+    unsigned char _monitorOutputBuffer[MONITOR_OUTPUT_BUFFER_LENGTH_MASK + 1];
+    unsigned short _monitorOutputBufferConsumerIndex;
+    unsigned short _monitorOutputBufferProducerIndex;
+
+    bool _writeMonitorChar(unsigned char c);
+
+    void _initOutputStageAckPacket(unsigned char packetId);
+    void _ackInputStageDataPacket(unsigned char packetId);
+    void _on_inputStageAckPacketComplete(unsigned char packetId);
+    void _on_inputStageDataPacketComplete(void);
+    void _processScsOutputStageIdle(void);
+    void _on_inputStageDataPacketComplete(void);
+
+	void _processScsInputStage();
+	void _processScsOutputStage();
+	void _processMonitorStage();
+
+    void printString(char * pString);
+    void printHex(unsigned char hex);
+    void printChar(unsigned char c);
+    void initScsDataExchange(void);
+
+    //// definitions, variables and functions above are about packetlization and reliable exchange, and are copied from A03_UART ///////////
+
+    // the following functions support 
+    bool _calculateCrc16(unsigned char * pData, unsigned char length, unsigned char * pCrcLow, unsigned char * pCrcHigh);
+    unsigned char _putCharsMonitor(unsigned char * pBuffer, unsigned char size);
+    unsigned short _getAppInputBufferAvailable(void);
+    unsigned short _writeAppInputBuffer(unsigned char * pBuffer, unsigned short length);
+    bool _getChar(unsigned char * p);
+    bool _putChar(unsigned char c);
+    unsigned char _putChars(unsigned char * pBuffer, unsigned char size);
+    unsigned short counter_get(void);
+    unsigned short counter_diff(unsigned short prevCounter);
+    unsigned short _readOutputBuffer(unsigned char * pBuffer, unsigned short size);
+    Poco::Mutex _mutex;
+    Poco::Timestamp _timeStamp;
+    CrcCcitt crc16;
+    IDataExchangeObserver * pDataObserver;
+    IPacketSender * pPacketSender;
+    IMonitorObserver * pMonitorObserver;
+    std::vector<unsigned char> dataVector;
+};
+
+
+
+#endif
